4cfc20aee2b368ae06b9225e33ca19ff0508d8e0
Damla Koksal <me@damlakoksal.com>
2023-07-31 18:25:35 +0300

    Revert "[#7]feat: add prisma orm"
    This reverts commit 8df2613c0711bc396c637a38c1b4dde8ef553860.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,6 @@
 import Image from 'next/image';
-import ListUsers from '@/components/list-users';
-import { Suspense } from 'react';
 
-export default async function Home() {
+export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
@@ -40,9 +38,6 @@ export default async function Home() {
           priority
         />
       </div>
-      <Suspense fallback={<div>Loading...</div>}>
-        <ListUsers />
-      </Suspense>
 
       <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
         <a
